016d45be3fbd6065d022a515d0c413aa04e658a4
Mitchell Tsutsulis <mitchell444999@gmail.com>
2022-05-30 22:03:32 +1000

    Added schafolding for map_surface, need draw using world_graph

---
+++ map_surface.py
@@ -0,0 +1,16 @@
+import pygame
+
+
+class MapSurface(pygame.Surface):
+    def __init__(self, map_size, map_color, location_color, world_graph):
+        super().__init__(map_size)
+        self.location_color = location_color
+        self.location_rect = pygame.Rect(0, 0, map_size[0] / 4, map_size[1] / 4)
+        self.location_rect.center = (map_size[0] / 2, map_size[1] / 2)
+        self.fill(map_color)
+
+        for location in world_graph.graph.key():
+            for connection in world_graph.graph[location]:
+                # Draw rects based on graph nodes, use:
+                # pygame.draw.rect(self, self.location_color, self.location_rect)
+                pass
