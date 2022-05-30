1461df5cf873a8a28635b6fabed701232f0df492
Mitchell Tsutsulis <mitchell444999@gmail.com>
2022-05-30 22:05:04 +1000

    Added testing code for map_surface

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import pygame
+
 from location import Location
 from world_graph import WorldGraph
 from direction import Direction
+from map_surface import MapSurface
 
 location_list = [
     Location("Home", "This is my home"),
@@ -15,3 +18,23 @@ connector_list = [
 ]
 
 world_graph = WorldGraph(location_list, "Home", connector_list)
+
+pygame.init()
+screen = pygame.display.set_mode((500, 500))
+
+map_size = (300, 300)
+map_color = (50, 50, 50)
+location_color = (100, 255, 100)
+
+map_surface = MapSurface(map_size, map_color, location_color, world_graph)
+
+gaming = True
+
+while gaming:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gaming = False
+    screen.blit(map_surface, (100, 100, 100, 100))
+    pygame.display.flip()
+
+pygame.quit()
