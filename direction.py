f8941f9df793089b0105488b1b6ad9a1caf67ff6
Mitchell Tsutsulis <mitchell444999@gmail.com>
2022-06-04 17:33:23 +1000

    Added to_coord func

--- direction.py
+++ direction.py
@@ -1,7 +1,19 @@
 from enum import Enum
 
+
 class Direction(Enum):
     UP = 1
     RIGHT = 2
     DOWN = 3
     LEFT = 4
+
+    def to_coord(self, factor=100):
+        match self:
+            case Direction.UP:
+                return 0, -1 * factor
+            case Direction.RIGHT:
+                return 1 * factor, 0
+            case Direction.DOWN:
+                return 0, 1 * factor
+            case Direction.LEFT:
+                return -1 * factor, 0
