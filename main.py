8cce9c1b5d6136d896656a952a157c88ae68e6a8
Mitchell Tsutsulis <mitchell444999@gmail.com>
2022-06-15 20:08:27 +1000

    Added changes to game init for small and big map init

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 
 from location import Location
+from screen import Screen
 from world_graph import WorldGraph
 from direction import Direction
 from map_surface import MapSurface
@@ -24,22 +25,29 @@ world_graph = WorldGraph(location_list, "Home", connector_list)
 
 pygame.init()
 screen_size = (1000, 720)  # Need to make tile spacing in map dependent on screen_size
-screen = pygame.display.set_mode(screen_size)
 
-map_size = (screen_size[0] / 4, screen_size[0] / 4)
+small_map_size = (screen_size[0] / 4, screen_size[0] / 4)
+big_map_size = (screen_size[0], screen_size[1])
 map_color = (50, 50, 50)
 location_color = (100, 255, 100)
 
-map_surface = MapSurface(map_size, map_color, location_color, world_graph)
+small_map_surface = MapSurface(small_map_size, map_color, location_color, world_graph, 75)
+big_map_surface = MapSurface(big_map_size, map_color, location_color, world_graph, 75)
 
+screen = Screen(screen_size, small_map_surface, (0, screen_size[1] - small_map_size[1]), big_map_surface)
 gaming = True
 
 while gaming:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gaming = False
-    # screen.blit(map_surface, ((screen_size[0] / 2) - map_size[0] / 2, (screen_size[1] / 2) - map_size[1] / 2))
-    screen.blit(map_surface, (0, screen_size[1] - map_size[1]))
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = pygame.mouse.get_pos()
+            if screen.small_map and screen.check_point_collide(small_map_surface, mouse_pos):
+                screen.toggle_map()
+            elif not screen.small_map and screen.check_point_collide(big_map_surface, mouse_pos):
+                screen.toggle_map()
+    screen.render()
     pygame.display.flip()
 
 pygame.quit()
